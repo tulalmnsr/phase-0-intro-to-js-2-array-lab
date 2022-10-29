4f520a96a5368204de63e34e8fab9807cb218dd8
Batoul El Mansour <elmansourbatoul@gmail.com>
2022-10-29 23:21:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+const name="cat"
+function destructivelyAppendCat(name){
+    cats.push(name);}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    const copycats=cats.slice();
+    copycats.push(name);
+    return copycats}
+function prependCat(name){
+    const copycats=cats.slice();
+    copycats.unshift(name);
+    return copycats}
+function   removeLastCat(){
+    const copycats=cats.slice();
+    copycats.pop();
+    return copycats
+}
+function removeFirstCat(){
+    const copycats=cats.slice();
+    copycats.shift();
+    return copycats
+}
